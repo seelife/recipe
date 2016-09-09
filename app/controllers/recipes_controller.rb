f73cd919167c3d7b2c7763d23eb3fed16f8dc46b
seelife <kmsims2@gmail.com>
2016-09-09 17:51:46 -0500

    created partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,43 @@ class RecipesController < ApplicationController
 
   end
 
+  def new
+    @recipe = Recipe.new
+
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(7)
+
+    if @recipe.save
+      flash[:success] = "Success! You added a recipe."
+      redirect_to recipes_path
+    else
+      render :new
+    end
 
 
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Success! You updated your recipe."
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+      def recipe_params
+        params.require(:recipe).permit(:name, :summary, :description)
+      end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,9 +1,12 @@
 class Chef < ApplicationRecord
   has_many :recipes
+
   before_save { self.email = email.downcase }
+
   validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
   VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 105},
                     uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
   #validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  
 end
